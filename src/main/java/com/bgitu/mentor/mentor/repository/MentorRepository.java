62d646f7c16f817f0bca305f013ff817332387b2
Quiltix <vanya200632@gmail.com>
2025-07-28 14:05:58 +0300

    refactor(mentor): add JpaSpecificationExecutor and delete ulesess methods

--- src/main/java/com/bgitu/mentor/mentor/repository/MentorRepository.java
+++ src/main/java/com/bgitu/mentor/mentor/repository/MentorRepository.java
@@ -4,27 +4,16 @@ import com.bgitu.mentor.mentor.model.Mentor;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
+
 import java.util.Optional;
 
-public interface MentorRepository extends JpaRepository<Mentor, Long> {
+public interface MentorRepository extends JpaRepository<Mentor, Long>, JpaSpecificationExecutor<Mentor> {
 
     boolean existsByEmail(@Email @NotBlank(message = "Email cannot be empty") String email);
 
     Optional<Mentor> findByEmail(String email);
 
-    List<Mentor> findTop3ByOrderByRankDesc();
 
-    List<Mentor> findAllBySpecialityId(Long specialityId);
-
-    List<Mentor> findBySpecialityIdOrderByRankDesc(Long specId);
-
-    @Query("SELECT m FROM Mentor m " +
-            "WHERE LOWER(m.firstName) LIKE LOWER(CONCAT('%', :query, '%')) " +
-            "   OR LOWER(m.lastName) LIKE LOWER(CONCAT('%', :query, '%')) " +
-            "   OR LOWER(m.description) LIKE LOWER(CONCAT('%', :query, '%'))")
-    List<Mentor> searchByNameOrDescription(@Param("query") String query);
 }
