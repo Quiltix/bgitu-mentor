9f885750a0a73cd09b93d7efd4064a4e33ce6028
Quiltix <vanya200632@gmail.com>
2025-07-29 17:54:50 +0300

    feat(vote): add BaseVote and interface

---
+++ src/main/java/com/bgitu/mentor/vote/data/model/BaseVote.java
@@ -0,0 +1,24 @@
+package com.bgitu.mentor.vote.data.model;
+
+
+import com.bgitu.mentor.user.model.BaseUser;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseVote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private boolean upvote;
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private BaseUser user;
+}
