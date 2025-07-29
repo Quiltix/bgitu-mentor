0d5454bcda4ef8a2ba6b5a943f2c538823b6ab38
Quiltix <vanya200632@gmail.com>
2025-07-29 17:19:00 +0300

    feat(article): add article specifications

---
+++ src/main/java/com/bgitu/mentor/article/data/ArticleSpecifications.java
@@ -0,0 +1,28 @@
+package com.bgitu.mentor.article.data;
+
+
+import com.bgitu.mentor.article.data.model.Article;
+import com.bgitu.mentor.mentor.data.model.Speciality;
+import jakarta.persistence.criteria.Join;
+import org.springframework.data.jpa.domain.Specification;
+
+public final class ArticleSpecifications {
+
+    public static Specification<Article> hasSpeciality(Long specialityId){
+        return  (root, query, criteriaBuilder) ->
+        {
+            Join<Article, Speciality> specialityJoin = root.join("speciality");
+            return criteriaBuilder.equal(specialityJoin.get("id"),specialityId);
+        };
+    }
+
+    public static Specification<Article> titleOrContentContains(String query) {
+        String likePattern = "%" + query.toLowerCase() + "%";
+        return (root, query1, criteriaBuilder) ->
+                criteriaBuilder.or(
+                        criteriaBuilder.like(criteriaBuilder.lower(root.get("title")), likePattern),
+                        criteriaBuilder.like(criteriaBuilder.lower(root.get("content")), likePattern)
+                );
+    }
+}
+
--- src/main/java/com/bgitu/mentor/article/data/repository/ArticleRepository.java
+++ src/main/java/com/bgitu/mentor/article/data/repository/ArticleRepository.java
@@ -2,12 +2,13 @@ package com.bgitu.mentor.article.data.repository;
 
 import com.bgitu.mentor.article.data.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
 
 
     List<Article> findAllByOrderByRankDesc();
