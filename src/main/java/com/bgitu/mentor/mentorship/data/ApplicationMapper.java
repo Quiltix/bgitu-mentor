4439c0f4a5355d0bed72f34e14f8db0025231939
Quiltix <vanya200632@gmail.com>
2025-08-07 12:51:23 +0300

    refactor(application): create mapper methods

--- src/main/java/com/bgitu/mentor/mentorship/data/ApplicationMapper.java
+++ src/main/java/com/bgitu/mentor/mentorship/data/ApplicationMapper.java
@@ -1,10 +1,18 @@
 package com.bgitu.mentor.mentorship.data;
 
 
-import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+import com.bgitu.mentor.mentorship.data.dto.ApplicationDetailsResponseDto;
+import com.bgitu.mentor.mentorship.data.model.Application;
+import com.bgitu.mentor.student.data.StudentMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {StudentMapper.class})
 public interface ApplicationMapper {
 
 
+
+    @Mapping(source = "student",target = "student")
+    ApplicationDetailsResponseDto toDetailsDto(Application application);
 }
