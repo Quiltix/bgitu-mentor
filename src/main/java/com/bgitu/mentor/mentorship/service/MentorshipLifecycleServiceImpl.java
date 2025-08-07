53f08abf8e66666d8290cf2519b0e787cfbec6e2
Quiltix <vanya200632@gmail.com>
2025-08-07 16:53:18 +0300

    refactor(mentorship): refactor links

--- src/main/java/com/bgitu/mentor/mentorship/service/MentorshipLifecycleServiceImpl.java
+++ src/main/java/com/bgitu/mentor/mentorship/service/MentorshipLifecycleServiceImpl.java
@@ -1,7 +1,6 @@
 package com.bgitu.mentor.mentorship.service;
 
 import com.bgitu.mentor.mentor.data.model.Mentor;
-import com.bgitu.mentor.mentor.data.repository.MentorRepository;
 import com.bgitu.mentor.student.data.model.Student;
 import com.bgitu.mentor.student.data.repository.StudentRepository;
 import jakarta.transaction.Transactional;
@@ -13,32 +12,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class MentorshipLifecycleServiceImpl implements  MentorshipLifecycleService{
 
-    private final MentorRepository mentorRepository;
     private final StudentRepository studentRepository;
 
 
     @Override
     @Transactional
     public void establishLink(Mentor mentor, Student student) {
-        // Здесь можно будет добавить сложную логику, например, проверку,
-        // что у студента нет другого ментора, или что у ментора есть свободные слоты.
-        student.setMentor(mentor);
-        // Важно поддерживать обе стороны связи для консистентности кэша Hibernate
-        mentor.getStudents().add(student);
 
-        // Сохранять не обязательно, если @Transactional управляет сессией,
-        // но явное сохранение делает код более понятным.
+        student.setMentor(mentor);
+
         studentRepository.save(student);
-        mentorRepository.save(mentor);
     }
 
     @Override
     @Transactional
     public void terminateLink(Mentor mentor, Student student) {
+
         student.setMentor(null);
-        mentor.getStudents().remove(student);
 
         studentRepository.save(student);
-        mentorRepository.save(mentor);
     }
 }
