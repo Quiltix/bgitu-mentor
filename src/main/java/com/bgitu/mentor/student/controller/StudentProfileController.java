e2c12cce7ada55104ca43c6daca089410edebd17
Quiltix <vanya200632@gmail.com>
2025-08-07 18:14:51 +0300

    refactor(student): refactor StudentProfileController

--- src/main/java/com/bgitu/mentor/student/controller/StudentProfileController.java
+++ src/main/java/com/bgitu/mentor/student/controller/StudentProfileController.java
@@ -11,7 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -23,66 +23,61 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @Tag(name = "Student Profile", description = "Методы для взаимодействия с профилем студента")
 @RequestMapping("/api/profiles/student")
+@PreAuthorize("hasRole('STUDENT')")
 public class StudentProfileController {
 
 
     private final StudentProfileService studentService;
 
-    @Operation(summary = "Получение карточки студента", description = "Доступно только для роли STUDENT. Возвращает полную информацию по текущему студенту.")
-    @PreAuthorize("hasRole('STUDENT')")
+    @Operation(summary = "Получить свою карточку", description = "Доступно только для роли STUDENT. Возвращает полную информацию по текущему студенту.")
     @GetMapping()
-    public ResponseEntity<StudentDetailsResponseDto> getCardStudent(Authentication authentication){
+    public StudentDetailsResponseDto getCardStudent(Authentication authentication){
         Long studentId = SecurityUtils.getCurrentUserId(authentication);
-        return ResponseEntity.ok(studentService.getPublicCardById(studentId));
+        return studentService.getPublicCardById(studentId);
     }
 
 
-    @Operation(summary = "Обновление карточки студента", description = "Доступно только для роли STUDENT. Позволяет редактировать описание и загрузить новый аватар.")
-    @PreAuthorize("hasRole('STUDENT')")
+    @Operation(summary = "Обновить свою карточку", description = "Доступно только для роли STUDENT. Позволяет редактировать описание и загрузить новый аватар.")
     @PatchMapping( consumes = "multipart/form-data")
-    public ResponseEntity<StudentDetailsResponseDto> updateStudentCard(
+    public StudentDetailsResponseDto updateStudentCard(
             Authentication authentication,
             @RequestPart("card") StudentDetailsUpdateRequestDto dto,
             @RequestPart(value = "avatar", required = false) MultipartFile avatarFile
     ) {
         Long studentId = SecurityUtils.getCurrentUserId(authentication);
-        return ResponseEntity.ok(studentService.updateCard(studentId, dto, avatarFile));
+        return studentService.updateCard(studentId, dto, avatarFile);
     }
 
-    @Operation(summary = "Получение моего профиля", description = "STUDENT. Возвращает профиль по авторизации")
-    @PreAuthorize("hasRole('STUDENT')")
-    @GetMapping("/settings")
-    public ResponseEntity<UserCredentialsResponseDto> getMentorProfile(Authentication authentication) {
+    @Operation(summary = "Получение своего профиля", description = "STUDENT. Возвращает профиль по авторизации")
+    @GetMapping("/credentials")
+    public UserCredentialsResponseDto getMentorProfile(Authentication authentication) {
         Long studentId = SecurityUtils.getCurrentUserId(authentication);
-        return ResponseEntity.ok(studentService.getPersonalInfo(studentId));
+        return studentService.getPersonalInfo(studentId);
     }
 
     @Operation(summary = "Обновление профиля студента", description = "Доступно только для роли STUDENT. Позволяет редактировать имя, фамилию и email.")
-    @PreAuthorize("hasRole('STUDENT')")
-    @PatchMapping("/settings")
-    public ResponseEntity<UserCredentialsResponseDto> updateMentorProfile(
+    @PatchMapping("/credentials")
+    public UserCredentialsResponseDto updateMentorProfile(
             Authentication authentication,
             @RequestBody @Valid UserCredentialsUpdateRequestDto dto
     ) {
         Long studentId = SecurityUtils.getCurrentUserId(authentication);
-        return ResponseEntity.ok(studentService.updateProfile(studentId, dto));
+        return studentService.updateProfile(studentId, dto);
     }
 
-    @PreAuthorize("hasRole('STUDENT')")
     @GetMapping("/mentor")
     @Operation(summary = "Получить информацию о менторе студента", description = "Доступно для роли STUDENT")
-    public ResponseEntity<MentorDetailsResponseDto> getStudentMentor(Authentication authentication) {
+    public MentorDetailsResponseDto getStudentMentor(Authentication authentication) {
         Long studentId = SecurityUtils.getCurrentUserId(authentication);
-        return ResponseEntity.ok(studentService.getMentorOfStudent(studentId));
+        return studentService.getMentorOfStudent(studentId);
     }
 
-    @DeleteMapping("/mentor") // <-- DELETE на ресурс "ментор" внутри профиля
-    @PreAuthorize("hasRole('STUDENT')")
+    @DeleteMapping("/mentor")
     @Operation(summary = "Прекратить менторство", description = "Позволяет студенту отказаться от своего текущего ментора.")
-    public ResponseEntity<Void> terminateMentorship(Authentication authentication) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void terminateMentorship(Authentication authentication) {
         Long studentId = SecurityUtils.getCurrentUserId(authentication);
         studentService.terminateCurrentMentorship(studentId);
-        return ResponseEntity.noContent().build();
     }
 
 }
