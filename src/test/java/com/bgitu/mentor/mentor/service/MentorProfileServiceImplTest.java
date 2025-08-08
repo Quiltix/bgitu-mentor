2712bb432412542eaedd31405fa98004fa5a8bd3
Quiltix <vanya200632@gmail.com>
2025-08-08 14:13:08 +0300

    test(mentor): add tests

--- src/test/java/com/bgitu/mentor/mentor/service/MentorProfileServiceImplTest.java
+++ src/test/java/com/bgitu/mentor/mentor/service/MentorProfileServiceImplTest.java
@@ -6,7 +6,9 @@ import com.bgitu.mentor.mentor.data.MentorMapper;
 import com.bgitu.mentor.mentor.data.model.Mentor;
 import com.bgitu.mentor.mentor.data.repository.MentorRepository;
 import com.bgitu.mentor.mentorship.service.MentorshipLifecycleService;
+import com.bgitu.mentor.speciality.data.model.Speciality;
 import com.bgitu.mentor.speciality.service.SpecialityService;
+import com.bgitu.mentor.student.data.model.Student;
 import com.bgitu.mentor.student.service.StudentDirectoryService;
 import com.bgitu.mentor.user.service.BaseUserManagementService;
 import com.bgitu.mentor.user.service.UserFinder;
@@ -25,12 +27,24 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class MentorProfileServiceImplTest {
 
-    private static Mentor createMentor(Long id, String firstName) {
+    private static Mentor createMentor(Long id) {
         Mentor mentor = new Mentor();
         mentor.setId(id);
-        mentor.setFirstName(firstName);
-
         return mentor;
+    }
+
+    private static Student createStudent(Long id, Mentor mentor) {
+        Student student = new Student();
+        student.setId(id);
+        student.setMentor(mentor);
+        return student;
+    }
+
+    private static Speciality createSpeciality(Long id, String name) {
+        Speciality speciality = new Speciality();
+        speciality.setId(id);
+        speciality.setName(name);
+        return speciality;
     }
 
     @Mock
@@ -56,7 +70,7 @@ class MentorProfileServiceImplTest {
 
 
     @Test
-    @DisplayName("Должен вернуть корректные статьи у ментора, если существуют")
+    @DisplayName("getMyArticles | Должен вернуть корректные статьи у ментора, если существуют")
     void getMyArticles_returnsArticlesDto(){
 
         Long mentorId = 1L;
@@ -86,7 +100,7 @@ class MentorProfileServiceImplTest {
 
 
     @Test
-    @DisplayName("Должен вернуть пустой список, если статей нет")
+    @DisplayName("getMyArticles | Должен вернуть пустой список, если статей нет")
     void getMyArticles_returnsEmptyList(){
 
         Long mentorId = 1L;
@@ -109,6 +123,77 @@ class MentorProfileServiceImplTest {
         verifyNoInteractions(studentDirectoryService,mentorshipLifecycleService);
 
     }
+
+    @Test
+    @DisplayName("terminateMentorship | Должен вызвать LifecycleService, если ментор является владельцем студента")
+    void terminateMentorshipWithStudent_shouldCallLifecycleService_whenMentorIsOwner(){
+
+        long mentorId = 0L;
+        long studentId = 1L;
+
+        Mentor mentor = createMentor(mentorId);
+
+        Student student = createStudent(studentId, mentor);
+
+        when(userFinder.findMentorById(mentorId)).thenReturn(mentor);
+
+        when(userFinder.findStudentById(studentId)).thenReturn(student);
+
+
+        mentorProfileService.terminateMentorshipWithStudent(mentorId,studentId);
+
+
+        verify(mentorshipLifecycleService, times(1)).terminateLink(mentor,student);
+    }
+
+    @Test
+    @DisplayName("terminateMentorship | Должен выбросить SecurityException, если у студента нет ментора")
+    void terminateMentorshipWithStudent_shouldThrowSecurityException_whenStudentHasNoMentor(){
+
+        long mentorId = 0L;
+        long studentId = 1L;
+
+        Mentor mentor = createMentor(mentorId);
+
+        Student student = new Student();
+        student.setId(studentId);
+
+        when(userFinder.findMentorById(mentorId)).thenReturn(mentor);
+
+        when(userFinder.findStudentById(studentId)).thenReturn(student);
+
+        assertThrows(SecurityException.class,() -> mentorProfileService.terminateMentorshipWithStudent(mentorId,studentId));
+
+        verify(mentorshipLifecycleService,never()).terminateLink(any(),any());
+    }
+
+    @Test
+    @DisplayName("terminateMentorship | Должен выбросить SecurityException, если у студента другой ментор")
+    void terminateMentorshipWithStudent_shouldThrowSecurityException_whenStudentHasAnotherMentor(){
+
+
+        long mentorId = 0L;
+        long mentorOwnerId = 20L;
+        long studentId = 1L;
+
+        Mentor mentor = createMentor(mentorId);
+
+        Mentor mentorOwner = createMentor(mentorOwnerId);
+
+        Student student = createStudent(studentId, mentorOwner);
+
+        when(userFinder.findMentorById(mentorId)).thenReturn(mentor);
+
+        when(userFinder.findStudentById(studentId)).thenReturn(student);
+
+        assertThrows(SecurityException.class,() -> mentorProfileService.terminateMentorshipWithStudent(mentorId,studentId));
+
+        verify(mentorshipLifecycleService,never()).terminateLink(any(),any());
+    }
+
+
+
+
 
 
 
