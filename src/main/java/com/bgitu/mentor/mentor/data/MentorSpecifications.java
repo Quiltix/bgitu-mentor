46929430785cb9bc1d92f65f90729cea209989c6
Quiltix <vanya200632@gmail.com>
2025-08-11 20:02:47 +0300

    refactor(mentor): simplify specification

--- src/main/java/com/bgitu/mentor/mentor/data/MentorSpecifications.java
+++ src/main/java/com/bgitu/mentor/mentor/data/MentorSpecifications.java
@@ -1,22 +1,17 @@
 package com.bgitu.mentor.mentor.data;
 
 import com.bgitu.mentor.mentor.data.model.Mentor;
-import com.bgitu.mentor.speciality.data.model.Speciality;
 import org.springframework.data.jpa.domain.Specification;
-import jakarta.persistence.criteria.Join;
 
 public final class MentorSpecifications {
 
   private MentorSpecifications() {
-    throw new IllegalStateException("Utility class");
+    throw new UnsupportedOperationException("Utility class");
   }
 
-  // Спецификация для фильтрации по специальности
   public static Specification<Mentor> hasSpeciality(Long specialityId) {
-    return (root, query, criteriaBuilder) -> {
-      Join<Mentor, Speciality> specialityJoin = root.join("speciality");
-      return criteriaBuilder.equal(specialityJoin.get("id"), specialityId);
-    };
+    return (root, query, criteriaBuilder) ->
+        criteriaBuilder.equal(root.join("speciality").get("id"), specialityId);
   }
 
   public static Specification<Mentor> nameOrDescriptionContains(String text) {
