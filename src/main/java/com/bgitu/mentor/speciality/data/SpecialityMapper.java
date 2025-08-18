23f6e5cdb1e5fdf89b93ce2d778f081d6bcb781d
Quiltix <vanya200632@gmail.com>
2025-08-18 10:44:15 +0300

    feat(mapstruct): add SpecialityMapper for converting Speciality to SpecialityDto

---
+++ src/main/java/com/bgitu/mentor/speciality/data/SpecialityMapper.java
@@ -0,0 +1,15 @@
+package com.bgitu.mentor.speciality.data;
+
+import com.bgitu.mentor.mentor.data.dto.SpecialityDto;
+import com.bgitu.mentor.speciality.data.model.Speciality;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SpecialityMapper {
+
+  SpecialityDto toDto(Speciality speciality);
+
+  List<SpecialityDto> toDtoList(List<Speciality> specialities);
+}
