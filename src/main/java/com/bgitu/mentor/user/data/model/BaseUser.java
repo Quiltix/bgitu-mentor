3e5eb98e792eae0cdc0fd767c14a911029df2e44
Quiltix <vanya200632@gmail.com>
2025-08-18 14:11:00 +0300

    refactor(user): remove validation annotations from email and password fields

--- src/main/java/com/bgitu/mentor/user/data/model/BaseUser.java
+++ src/main/java/com/bgitu/mentor/user/data/model/BaseUser.java
@@ -1,8 +1,6 @@
 package com.bgitu.mentor.user.data.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
