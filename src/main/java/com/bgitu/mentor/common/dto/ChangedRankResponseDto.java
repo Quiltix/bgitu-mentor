a2f17c5a4451549f865648e08aff070c2f77065d
Quiltix <vanya200632@gmail.com>
2025-09-09 11:52:39 +0300

    feat(vote): create ChangedRankResponseDto

---
+++ src/main/java/com/bgitu/mentor/common/dto/ChangedRankResponseDto.java
@@ -0,0 +1,12 @@
+package com.bgitu.mentor.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ChangedRankResponseDto {
+  private Integer rank;
+}
