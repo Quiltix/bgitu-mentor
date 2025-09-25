a8687d012de14dc8d27e16975d397152f62f17e0
Quiltix <vanya200632@gmail.com>
2025-09-25 12:17:45 +0300

    feat(vote): add repository method

--- src/main/java/com/bgitu/mentor/vote/data/repository/MentorVoteRepository.java
+++ src/main/java/com/bgitu/mentor/vote/data/repository/MentorVoteRepository.java
@@ -3,7 +3,11 @@ package com.bgitu.mentor.vote.data.repository;
 import com.bgitu.mentor.vote.data.model.MentorVote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MentorVoteRepository extends JpaRepository<MentorVote, Long> {
 
   boolean existsByMentorIdAndUserId(Long entityId, Long id);
+
+  Optional<MentorVote> findByUser_IdAndMentor_Id(Long id, Long id1);
 }
