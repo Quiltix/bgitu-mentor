868dc2e82963a9e613d6fa539d144ad37d2c5081
Quiltix <vanya200632@gmail.com>
2025-10-17 12:34:27 +0300

    test(mentor): add test for update credentials

--- src/test/java/com/bgitu/mentor/mentor/controller/MentorProfileControllerIntegrationTest.java
+++ src/test/java/com/bgitu/mentor/mentor/controller/MentorProfileControllerIntegrationTest.java
@@ -4,20 +4,26 @@ import com.bgitu.mentor.mentor.data.model.Mentor;
 import com.bgitu.mentor.mentor.data.repository.MentorRepository;
 import com.bgitu.mentor.speciality.data.model.Speciality;
 import com.bgitu.mentor.testsupport.AbstractIntegrationTest;
+import com.bgitu.mentor.user.data.dto.UserCredentialsUpdateRequestDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class MentorProfileControllerIntegrationTest extends AbstractIntegrationTest {
 
   @Autowired private MentorRepository mentorRepository;
+  @Autowired private PasswordEncoder passwordEncoder;
 
   @BeforeEach
   void setUp() {
@@ -73,5 +79,38 @@ class MentorProfileControllerIntegrationTest extends AbstractIntegrationTest {
     resultActions
         .andExpect(status().isNotFound())
         .andExpect(jsonPath("$.message").value("Пользователь не найден"));
+  }
+
+  @Test
+  @DisplayName("GET api/mentors/credentials | should return 200 and updated data")
+  @WithMockUser(roles = "MENTOR")
+  void updateMentorProfile_shouldReturn200AndUpdatedData() throws Exception {
+    Mentor fakeMentor =
+        createMentor("Ivan", "Ivanovich", "123@test.com", null, null, null, "12345678");
+    mentorRepository.save(fakeMentor);
+
+    UserCredentialsUpdateRequestDto fakeDto = new UserCredentialsUpdateRequestDto();
+    fakeDto.setEmail("new@test.com");
+    fakeDto.setPassword("newPassword");
+
+    String fakeDtoString = objectMapper.writeValueAsString(fakeDto);
+
+    mockMvc
+        .perform(
+            patch("api/mentors/credentials")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(fakeDtoString))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.email").value(fakeDto.getEmail()));
+
+    Mentor updatedMentor =
+        mentorRepository
+            .findById(0L)
+            .orElseThrow(() -> new AssertionError("Mentor should exist in database"));
+
+    assertThat(updatedMentor.getEmail()).isEqualTo(fakeDto.getEmail());
+
+    assertThat(passwordEncoder.matches(fakeDto.getPassword(), updatedMentor.getPassword()))
+        .isTrue();
   }
 }
